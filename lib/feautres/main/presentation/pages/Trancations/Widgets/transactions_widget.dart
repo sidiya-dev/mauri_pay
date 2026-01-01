4876729e58d40df6df9c102f64068fb17c71523f
befine49 <lvloujeh@outlook.com>
2026-01-01 23:22:17 +0000

    transactions_widgets

---
+++ lib/feautres/main/presentation/pages/Trancations/Widgets/transactions_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+enum TransactionType {INCOME,OUTCOME}
+class TransactionsWidget extends StatelessWidget {
+  TransactionsWidget({super.key,required this.type});
+  final formatter = NumberFormat.currency(symbol:"\$");
+
+  final TransactionType type;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 18,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  child: Icon(type==TransactionType.INCOME ? Icons.arrow_outward:Icons.subdirectory_arrow_right),
+                  backgroundColor: type==TransactionType.INCOME? Colors.green:Colors.redAccent ,
+              ), 
+              SizedBox(width: 20.0,),              
+              Text(type==TransactionType.INCOME ?"Income":"Outcome")          
+            ],           
+            ),
+            Text(formatter.format(type==TransactionType.INCOME?544.42:-290.11),
+                  style: TextStyle(
+                    color: type==TransactionType.INCOME?Colors.green:Colors.redAccent,
+                    fontSize: 16, 
+                  ),
+            ),
+        ],
+      
+      ),
+                  
+    );
+  }
+}
