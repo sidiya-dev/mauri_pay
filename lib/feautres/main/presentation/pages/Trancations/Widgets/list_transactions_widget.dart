915d400c12b1612d1a5cd730ead2299132b2ade7
befine49 <lvloujeh@outlook.com>
2026-01-02 05:37:51 +0000

    List of transactions complated

--- lib/feautres/main/presentation/pages/Trancations/Widgets/list_transactions_widget.dart
+++ lib/feautres/main/presentation/pages/Trancations/Widgets/list_transactions_widget.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-enum TransactionType {INCOME,OUTCOME}
+enum ListTransactionType {INCOME,OUTCOME}
 class ListTransactionsWidget extends StatelessWidget {
   ListTransactionsWidget({super.key,required this.type});
   final formatter = NumberFormat.currency(symbol:"\$");
 
-  final TransactionType type;
+  final ListTransactionType type;
   
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(8),
-      margin: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(9),
-        color: const Color.fromARGB(255, 84, 85, 86),
-      ),
+      padding: EdgeInsets.symmetric(horizontal:7.0 ,vertical:10.0 ),
       child: Row(
-        children: [
+        children: [   
             CircleAvatar(
-              child: Icon(type==TransactionType.INCOME ? Icons.arrow_outward:Icons.subdirectory_arrow_right),
-              backgroundColor: type==TransactionType.INCOME? Colors.green:Colors.redAccent ,
+              child: Icon(type==ListTransactionType.INCOME ? Icons.arrow_outward:Icons.subdirectory_arrow_right),
+              backgroundColor: type==ListTransactionType.INCOME? Colors.green:Colors.redAccent ,
+            ),
+            SizedBox(width: 10.0,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Salary Deposit"),
+                    Row(
+                      children: [
+                        Text(type==ListTransactionType.INCOME ?"Income":"Outcome"),
+                        Text(". Dec 25, 2025")
+                      ],
+                    ),          
+                  ],
+                ),
+                SizedBox(width: 75,),
+                Text(formatter.format(type==ListTransactionType.INCOME?544.42:-290.11),
+                  style: TextStyle(
+                    color: type==ListTransactionType.INCOME?Colors.green:Colors.redAccent,
+                    fontSize: 18, 
+                  ),
+                ),
+              ],
+              
+            ),
+      ],
 
-            )
-      ],),            
+      ),            
     );
   }
 }
--- lib/feautres/main/presentation/pages/Trancations/transactions_page.dart
+++ lib/feautres/main/presentation/pages/Trancations/transactions_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mauri_pay/feautres/main/presentation/pages/Trancations/Widgets/list_transactions_widget.dart';
 import 'package:mauri_pay/feautres/main/presentation/pages/Trancations/Widgets/transactions_widget.dart';
 class TransactionsPage extends StatelessWidget {
   const TransactionsPage({super.key});
@@ -6,30 +7,83 @@ class TransactionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          Container(
-            width: 165.0,
-            padding:EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color.fromARGB(107, 95, 97, 98),
-            ),
-            child:TransactionsWidget(type: TransactionType.OUTCOME),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 165.0,
+                padding:EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(107, 95, 97, 98),
+                ),
+                child:TransactionsWidget(type: TransactionType.OUTCOME),
+              ),
+              Container(
+                width: 165.0,
+                padding:EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                 decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(107, 95, 97, 98),
+                ),
+                child:TransactionsWidget(type: TransactionType.INCOME),
+              ),
+          
+          
+            ],
           ),
-          Container(
-            width: 165.0,
-            padding:EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color.fromARGB(107, 95, 97, 98),
-            ),
-            child:TransactionsWidget(type: TransactionType.INCOME),
+          SizedBox(height: 20.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Recent Transactions ",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              ElevatedButton(onPressed: () {
+                
+              }, child: Text(" See All",style: TextStyle( color: Colors.blue),
+              ),
+              ),
+            ],
           ),
+          SizedBox(height: 20.0,),
+          Text("Recent Transactions ",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
+          Container(
+            margin: EdgeInsets.all(7.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(9),
+              color: const Color.fromARGB(255, 84, 85, 86),
+            ),
+            child: Column(
+                children: [
+                  ListTransactionsWidget(type: ListTransactionType.INCOME),
+                  ListTransactionsWidget(type: ListTransactionType.OUTCOME),
+                  ListTransactionsWidget(type: ListTransactionType.INCOME),
+                  ListTransactionsWidget(type: ListTransactionType.OUTCOME),
+                  ListTransactionsWidget(type: ListTransactionType.INCOME),
+                  ListTransactionsWidget(type: ListTransactionType.OUTCOME),
+                  ListTransactionsWidget(type: ListTransactionType.INCOME),
+                  ListTransactionsWidget(type: ListTransactionType.OUTCOME),
+                ],
+            ),
+          ),  
         ],
+      
       ),
+    
+
+      
     );
   }
 }
